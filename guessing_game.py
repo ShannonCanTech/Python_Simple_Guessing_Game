cead1de90c8843332bd3ff3f410165928b81b71c
shannoncantech <LdwpEtegOLLon9#1>
2018-06-06 01:18:32 -0400

    Initial commit

---
+++ guessing_game.py
@@ -0,0 +1,12 @@
+print("I'm think of a number between 1 and 10.")
+guess = int(input("Input a number: "))
+# print(guess)
+
+from random import randint
+game = randint(0, 10)
+
+if game == guess :
+    print("You got it right!")
+    print(str(game) + " was the correct answer")
+else :
+    print("Sorry. " + str(guess) + " Isn't correct. Try again.")
